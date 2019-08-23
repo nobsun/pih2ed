51040eba8a9540c1874273aa0455ecfa1838a3d5
Nobuo Yamashita <nobsun@sampou.org>
2019-08-24 07:16:09 +0900

    add Ch02Ex01.hs

---
+++ src/Ch02Ex01.hs
@@ -0,0 +1,70 @@
+{- |
+Module      : Ch02Ex01
+Description : 『プログラミングHaskell第2版』練習問題 2.1
+Copyright   : (c) Nobuo Yamashita, 2019
+License     : BSD-3
+Maintainer  : nobsun@gmail.com
+
+この章の例題をGHCiを用いて実行してください
+-}
+module Ch02Ex01 where
+
+{- $setup
+>>> 2+3*4
+14
+
+>>> (2+3)*4
+20
+
+>>> sqrt (3^2+4^2)
+5.0
+
+>>> head [1,2,3,4,5]
+1
+
+>>> tail [1,2,3,4,5]
+[2,3,4,5]
+
+>>> [1,2,3,4,5] !! 2
+3
+
+>>> take 3 [1,2,3,4,5]
+[1,2,3]
+
+>>> drop 3 [1,2,3,4,5]
+[4,5]
+
+>>> length [1,2,3,4,5]
+5
+
+>>> sum [1,2,3,4,5]
+15
+
+>>> product [1,2,3,4,5]
+120
+
+>>> [1,2,3] ++ [4,5]
+[1,2,3,4,5]
+
+>>> reverse [1,2,3,4,5]
+[5,4,3,2,1]
+
+>>> :load src/test.hs
+
+>>> quadruple 10
+40
+
+>>> take (double 2) [1,2,3,4,5]
+[1,2,3,4]
+
+>>> :set editor emacs
+
+>>> :edit
+
+>>> factorial 10
+3628800
+
+>>> average [1,2,3,4,5]
+3
+
+-}
---
+++ src/test.hs
@@ -0,0 +1,11 @@
+double x = x + x
+
+quadruple x = double (double x)
+
+{- -}
+factorial n = product [1 .. n]
+
+average ns = sum ns `div` length ns
+-- -}
+
+
