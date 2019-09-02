e27fa9244ac8f74822c2d99ec6139ddcec310e74
Nobuo Yamashita <nobsun@sampou.org>
2019-09-02 21:45:06 +0900

    add ex9.2

--- src/Ch09/Ex02.hs
+++ src/Ch09/Ex02.hs
@@ -5,21 +5,24 @@ Copyright   : (c) Nobuo Yamashita, 2019
 License     : BSD-3
 Maintainer  : nobsun@gmail.com
 
-関数 @add@ と同様に自然数の乗算関数 @mult :: Nat -> Nat -> Nat@ を再帰的に定義してください．
+再帰的な関数 @isChoice :: Eq a => [a] -> [a] -> Bool@ を定義してください．
+この関数は @perms@ や @subs@ を使わずに，一方のリストが他方のリストから選択されたものかを検査します．
 -}
 module Ch09.Ex02 where
 
-import Ch09.CountDown hiding (choices)
+{- |
+  最初に見つけた特定の要素をリストから取り除く
+-}
+removeone :: Eq a => a -> [a] -> [a]
+removeone _ [] = []
+removeone x (y:ys)
+  | x == y     = ys
+  | otherwise  = y : removeone x ys
 
 {- |
-  選択肢の生成
-
-@
-  choices xs = [ zs | ys <- subs xs, zs <- perms ys ]
-@
-
->>> choices [1,2,3]
-[[],[3],[2],[2,3],[3,2],[1],[1,3],[3,1],[1,2],[2,1],[1,2,3],[2,1,3],[2,3,1],[1,3,2],[3,1,2],[3,2,1]]
+  一方が他方のリストから選択さたものかをチェックする
 -}
-choices :: [a] -> [[a]]
-choices xs = [ zs | ys <- subs xs, zs <- perms ys ]
+isChoice :: Eq a => [a] -> [a] -> Bool
+isChoice [] _      = True
+isChoice (_:_) []  = False
+isChoice (x:xs) ys = x `elem` ys && isChoice xs (removeone x ys)
