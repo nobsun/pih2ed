2415a9abd5d8a6a10b6eb9f0cac9a9eaa5a06f32
Nobuo Yamashita <nobsun@sampou.org>
2019-09-04 22:45:50 +0900

    add ex10.4

---
+++ src/Ch10/Ex04.hs
@@ -0,0 +1,38 @@
+{- |
+Module      : Ch10.Ex04
+Description : 練習問題 10.4
+Copyright   : (c) Nobuo Yamashita, 2019
+License     : BSD-3
+Maintainer  : nobsun@gmail.com
+
+指定した数だけキーボードから数字を読み取り，それらの和を表示する @adder :: IO ()@ を定義してください．
+-}
+module Ch10.Ex04 where
+
+import Data.Char
+{- |
+  加算器
+
+>>> adder
+How many numbers? 5
+1
+3
+5
+7
+9
+The total is 25
+-}
+adder :: IO ()
+adder = do
+  s <- getLine
+  adder' 0 (read s)
+
+{- |
+  補助関数
+-}
+adder' :: Int -> Int -> IO ()
+adder' a 0 = putStrLn (show a)
+adder' a n = do
+  c <- getChar
+  adder' (a + digitToInt c) (n - 1)
+
